77dee41ec5db908fddc7bfb08fb0ac8df380b4f8
Edward Hurley <60479814+echurley@users.noreply.github.com>
2022-04-07 17:36:08 -0400

    Added Mona Lisa and Great Wave

--- im2circles.m
+++ im2circles.m
@@ -2,15 +2,15 @@
 
 clear; close;
 
-im = imread('PearlEarring.jpg');
+im = imread('GreatWave.jpg');
 im = im2double(im);
 
 %% Convert Color Space
 
-custom = [1 1 0; 0 0 1; 1 0 0]';
+custom = [0 1 1; 1 0 0]';
 out = rgb2custom(im,custom,2048);
 out = medfilt3(out,[7,7,1],'symmetric');
-out = imresize(out,2.28);
+out = imresize(out,2.446);
 
 %% Create Edge Map
 
@@ -35,16 +35,20 @@ circles = zeros(size(out));
 radius = max(dist1,[],[1,2],'linear');
 [x,y] = meshgrid(1:size(out,2),1:size(out,1),1:size(out,3));
 
-while mean2(radius) >= 5
+while mean2(radius) >= 1
     
+    tic
     [radius,C] = max(dist1,[],[1 2],'linear');
     mask = (y - y(C)).^2 + (x - x(C)).^2;
     dist1 = min((mask - radius.^2) ./ (dist1 + 2 * radius),dist1);
-    
+    toc
+
+    tic
     mask = 1 - (mask - radius.^2) ./ radius;
     mask = min(max(mask,1),2) - 1;
     color = sum(out .* mask,[1,2]) ./ sum(mask,[1,2]);
     circles = circles + color.^2 .* mask;
+    toc
     
 end
 
@@ -55,5 +59,5 @@ for i = 1:size(circles,3)
     im2 = im2 + out(:,:,i) .* reshape(custom(:,i),[1 1 3]);
 end
 
-imwrite(RGB,'pearlCircles5.png','png')
+imwrite(RGB,'waveCircles.png','png')
 imshow(RGB)
